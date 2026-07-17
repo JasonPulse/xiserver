75e1731c0d89832dc266c1c15a039d08fcacea7d
Jason Clift <jason@pulsegroupb2b.com>
2026-07-17 14:36:19 -0700

    fix(trusts): checks

--- scripts/actions/spells/trust/tenzen_ii.lua
+++ scripts/actions/spells/trust/tenzen_ii.lua
@@ -21,7 +21,6 @@ spellObject.onMobSpawn = function(mob)
     mob:addGambit(ai.t.SELF, { ai.c.HAS_TOP_ENMITY, 0 },                 { ai.r.JA, ai.s.SPECIFIC, xi.ja.THIRD_EYE })
     mob:addGambit(ai.t.SELF, { ai.c.TP_LT, 1000 },                       { ai.r.JA, ai.s.SPECIFIC, xi.ja.MEDITATE })
     mob:addGambit(ai.t.SELF, { ai.c.NOT_STATUS, xi.effect.WARDING_CIRCLE }, { ai.r.JA, ai.s.SPECIFIC, xi.ja.WARDING_CIRCLE })
-
 end
 
 spellObject.onMobDespawn = function(mob)
